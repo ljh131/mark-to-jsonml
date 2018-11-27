9804f5b690112d1b95e3d1f00ad45af953e671eb
ljh131 <ljh131@gmail.com>
2018-11-27 17:54:33 +0900

    use webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: './src/parser.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'parser.js',
+    library: "mark-to-jsonml",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            "plugins": [
+              ["@babel/plugin-proposal-decorators", { "legacy": true }],
+              ["@babel/plugin-proposal-class-properties"],
+              ["babel-plugin-transform-remove-console"]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
